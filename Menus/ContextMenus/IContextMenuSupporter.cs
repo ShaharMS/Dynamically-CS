c6de9214b51aa8f33d5f20a870c79c747d3bb6cc
Shahar Marcus <88977041+ShaharMS@users.noreply.github.com>
2023-07-11 00:48:42 +0300

    a whole lotta progress today

---
+++ Menus/ContextMenus/IContextMenuSupporter.cs
@@ -0,0 +1,13 @@
+﻿using Dynamically.Backend.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dynamically.Menus.ContextMenus;
+
+public interface IContextMenuSupporter
+{
+    public List<DraggableGraphic> PartOf { get; set; }
+}
