47fd17d57b06dd8522b028dcea4772b09d2c02e1
Shahar Marcus <88977041+ShaharMS@users.noreply.github.com>
2023-08-01 12:57:53 +0300

    prgress

--- Backend/Helpers/RoleMap_Joint.cs
+++ Backend/Helpers/RoleMap_Joint.cs
@@ -14,30 +14,30 @@ namespace Dynamically.Backend.Helpers;
 
 public partial class RoleMap
 {
-    private void Joint__AddToRole<T>(Role role, T item, Joint joint)
+    private void Joint__AddToRole<T>(Role role, T item, Joint Subject)
     {
         switch (role)
         {
             // Ray
             case Role.RAY_On:
-                (item as RayFormula).AddFollower(joint);
+                (item as RayFormula).AddFollower(Subject);
                 break;
             // Segment
             case Role.SEGMENT_Corner:
                 var s1 = item as Segment;
-                joint.OnMoved.Add(s1.__updateFormula);
-                joint.OnDragged.Add(s1.__reposition);
+                Subject.OnMoved.Add(s1.__updateFormula);
+                Subject.OnDragged.Add(s1.__reposition);
                 break;
             // Circle
             case Role.CIRCLE_On:
-                (item as Circle).Formula.AddFollower(joint);
+                (item as Circle).Formula.AddFollower(Subject);
                 break;
             case Role.CIRCLE_Center:
-                joint.OnMoved.Add((item as Circle).__circle_OnChange);
+                Subject.OnMoved.Add((item as Circle).__circle_OnChange);
                 break;
             // Triangle
             case Role.TRIANGLE_Corner:
-                joint.OnRemoved.Add((_, _) => (item as Triangle).Dismantle());
+                Subject.OnRemoved.Add((_, _) => (item as Triangle).Dismantle());
                 break;
             default: break;
         }
