2c7de6e63c2f5b8336618260808ac72ee98a4a96
Shahar Marcus <88977041+ShaharMS@users.noreply.github.com>
2023-09-29 14:45:51 +0300

    starting with the auto-solver! just framework rn. AI is next :)

---
+++ Solver/Information/BuildingBlocks/TVertex.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dynamically.Solver.Information.BuildingBlocks
+{
+    internal class TVertex
+    {
+    }
+}
---
+++ Solver/Structs/SVertexOnSegmentRatio.cs
@@ -0,0 +1,26 @@
+﻿using Dynamically.Solver.Information;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dynamically.Solver.Structs;
+
+public struct SVertexOnSegmentRatio
+{
+    public TVertex Vertex;
+
+    public TSegment Segment;
+
+    public double RatioV1;
+    public double RatioV2;
+
+    public SVertexOnSegmentRatio(TVertex v, TSegment s, double? rv1, double? rv2)
+    {
+        Vertex = v;
+        Segment = s;
+        RatioV1 = (double)(rv1 != null ? rv1 : rv2 != null ? 1 - rv2 : throw new ArgumentException("rv1/rv2 must be non-null, and the other null. both are null"));
+        RatioV2 = 1 - RatioV1;
+    }
+}
