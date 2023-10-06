2d6c689bd108131ce00ab5a54dcfd771f88f552f
Shahar Marcus <88977041+ShaharMS@users.noreply.github.com>
2023-10-06 11:26:41 +0300

    one more reason

--- SolverSubProject/Details/Relation.cs
+++ SolverSubProject/Details/Relation.cs
@@ -9,105 +9,117 @@ namespace Dynamically.Solver.Details;
 public enum Relation
 {
     /// <summary>
-    /// <list>
+    /// <list type="bullet">
     ///     <item>Segment, Segment</item>
     ///     <item>No side products</item>
     /// </list>
     /// </summary>
     CONNECTED,
     /// <summary>
-    /// <list>
+    /// <list type="bullet">
     ///     <item>Vertex, Segment | Vertex, Circle</item>
     ///     <item>No side products</item>
     /// </list>
     /// </summary>
     ON,
     /// <summary>
-    /// <list>
+    /// <list type="bullet">
     ///     <item>Vertex, Segment</item>
     ///     <item>No side products</item>
     /// </list>
     /// </summary>
     MIDDLE,
     /// <summary>
-    /// <list>
+    /// <list type="bullet">
     ///     <item>Segment, Segment | Vertex, Segment</item>
     ///     <item>On <c>Segment, Segment</c>, one side product - point of intersection</item>
     /// </list>
     /// </summary>
     INTERSECTS,
     /// <summary>
-    /// <list>
+    /// <list type="bullet">
     ///     <item>Segment, Segment | Segment, Angle</item>
     ///     <item>On <c>Segment, Segment</c>, one side product - point of bisection</item>
     /// </list>
     /// </summary>
     BISECTS,
     /// <summary>
-    /// <list>
+    /// <list type="bullet">
     ///     <item>Segment, Circle</item>
     ///     <item>One side product - point of intersection</item>
     /// </list>
     /// </summary>
     TANGENT,
     /// <summary>
-    /// <list>
+    /// <list type="bullet">
     ///     <item>Segment, Segment</item>
     ///     <item>No side products</item>
     /// </list>
     /// </summary>
     PARALLEL,
     /// <summary>
-    /// <list>
+    /// <list type="bullet">
+    ///     <item>Segment, Triangle, Segment, Quad</item>
+    ///     <item> Two scenarios:
+    ///         <list type="bullet">
+    ///             <item>On <c>Segment, Triangle</c>, one side product - the other side</item>
+    ///             <item>On <c>Segment, Quad</c>, two side products - the intersecting sides</item>
+    ///         </list>
+    ///     </item>
+    /// </list>
+    /// </summary>
+    MIDSEGMENT,
+    /// <summary>
+    /// <list type="bullet">
     ///     <item>Segment, Segment</item>
     ///     <item>One side product - point of intersection</item>
     /// </list>
     /// </summary>
     PERPENDICULAR,
     /// <summary>
-    /// <list>
+    /// <list type="bullet">
     ///     <item>Vertex | Segment | Circle</item>
     ///     <item>No side products</item>
     /// </list>
     /// </summary>
     AUXILIARY,
     /// <summary>
-    /// <list>
+    /// <list type="bullet">
     ///     <item>Segment, Segment | Segment, Value | Angle, Angle | Angle, Value | Angle, Arc | Arc, Arc | Arc, Value | Value, Value </item>
     ///     <item>0 or more side products - left operand can be equal to one or more elements</item>
     /// </list>
     /// </summary>
     EQUALS,
     /// <summary>
-    /// <list>
+    /// <list type="bullet">
     ///     <item>Segment, Segment | Segment, Value | Angle, Angle | Angle, Value | Angle, Arc | Arc, Arc | Arc, Value | Value, Value </item>
     ///     <item>0 or more side products - left operand can be unequal to one or more elements</item>
     /// </list>
     /// </summary>
     NOTEQUALS,
     /// <summary>
-    /// <list>
+    /// <list type="bullet">
     ///     <item>Segment, Segment | Segment, Value | Angle, Angle | Angle, Value | Angle, Arc | Arc, Arc | Arc, Value | Value, Value </item>
     ///     <item>0 or more side products - left operand can be smaller than one or more elements</item>
     /// </list>
     /// </summary>
     LARGER,
     /// <summary>
-    /// <list>
+    /// <list type="bullet">
     ///     <item>Segment, Segment | Segment, Value | Angle, Angle | Angle, Value | Angle, Arc | Arc, Arc | Arc, Value | Value, Value </item>
     ///     <item>0 or more side products - left operand can be larger than one or more elements</item>
     /// </list>
     /// </summary>
     SMALLER,
     /// <summary>
-    /// <list>
+    /// <list type="bullet">
     ///     <item>Segment, Segment | Segment, Value | Angle, Angle | Angle, Value | Angle, Arc | Arc, Arc | Arc, Value | Value, Value </item>
     ///     <item>0 or more side products - left operand can be equal/larger than one or more elements</item>
     /// </list>
     /// </summary>
     EQLARGER,
     /// <summary>
-    /// <list>
+    /// <list type="bullet">
     ///     <item>Segment, Segment | Segment, Value | Angle, Angle | Angle, Value | Angle, Arc | Arc, Arc | Arc, Value | Value, Value </item>
     ///     <item>0 or more side products - left operand can be equal/smaller than one or more elements</item>
     /// </list>
@@ -115,14 +127,14 @@ public enum Relation
     EQSMALLER,
 
     /// <summary>
-    /// <list>
+    /// <list type="bullet">
     ///     <item>Triangle, Triangle</item>
     ///     <item>No side products</item>
     /// </list>
     /// </summary>
     CONGRUENT,
     /// <summary>
-    /// <list>
+    /// <list type="bullet">
     ///     <item>Triangle, Triangle</item>
     ///     <item>No side products</item>
     /// </list>
@@ -130,7 +142,7 @@ public enum Relation
     SIMILAR,
 
     /// <summary>
-    /// <list>
+    /// <list type="bullet">
     ///     <item>Vertex</item>
     ///     <item>No side products</item>
     /// </list>
@@ -139,28 +151,28 @@ public enum Relation
 
 
     /// <summary>
-    /// <list>
+    /// <list type="bullet">
     ///     <item>Triangle</item>
     ///     <item>No side products</item>
     /// </list>
     /// </summary>
     TRIANGLE_EQUILATERAL,
     /// <summary>
-    /// <list>
+    /// <list type="bullet">
     ///     <item>Triangle</item>
     ///     <item>Two side product - the similar sides</item>
     /// </list>
     /// </summary>
     TRIANGLE_ISOSCELES,
     /// <summary>
-    /// <list>
+    /// <list type="bullet">
     ///     <item>Triangle</item>
     ///     <item>One side product - the right angle</item>
     /// </list>
     /// </summary>
     TRIANGLE_RIGHT,
     /// <summary>
-    /// <list>
+    /// <list type="bullet">
     ///     <item>Triangle</item>
     ///     <item>No side products</item>
     /// </list>
@@ -168,63 +180,63 @@ public enum Relation
     TRIANGLE_SCALENE,
 
     /// <summary>
-    /// <list>
+    /// <list type="bullet">
     ///     <item>Quad</item>
     ///     <item>No side products</item>
     /// </list>
     /// </summary>
     QUAD_SQUARE,
     /// <summary>
-    /// <list>
+    /// <list type="bullet">
     ///     <item>Quad</item>
     ///     <item>4 side products - the first pair of similar sides, then the second</item>
     /// </list>
     /// </summary>
     QUAD_RECTANGLE,
     /// <summary>
-    /// <list>
+    /// <list type="bullet">
     ///     <item>Quad</item>
     ///     <item>4 side products - the first pair of parallel sides, then the second</item>
     /// </list>
     /// </summary>
     QUAD_PARALLELOGRAM,
     /// <summary>
-    /// <list>
+    /// <list type="bullet">
     ///     <item>Quad</item>
     ///     <item>No side products</item>
     /// </list>
     /// </summary>
     QUAD_RHOMBUS,
     /// <summary>
-    /// <list>
+    /// <list type="bullet">
     ///     <item>Quad</item>
     ///     <item>2 side products - the two parallel segments</item>
     /// </list>
     /// </summary>
     QUAD_TRAPEZOID,
     /// <summary>
-    /// <list>
+    /// <list type="bullet">
     ///     <item>Quad</item>
     ///     <item>4 side products - the two parallel segments, then the 2 similar sides</item>
     /// </list>
     /// </summary>
     QUAD_ISOSCELES_TRAPEZOID,
     /// <summary>
-    /// <list>
+    /// <list type="bullet">
     ///     <item>Quad</item>
     ///     <item>3 side products - the two parallel segments, then the right angle</item>
     /// </list>
     /// </summary>
     QUAD_RIGHT_TRAPEZOID,
     /// <summary>
-    /// <list>
+    /// <list type="bullet">
     ///     <item>Quad</item>
     ///     <item>2 side products - the two head angles</item>
     /// </list>
     /// </summary>
     QUAD_KITE,
     /// <summary>
-    /// <list>
+    /// <list type="bullet">
     ///     <item>Quad</item>
     ///     <item>No side products</item>
     /// </list>
@@ -232,7 +244,7 @@ public enum Relation
     QUAD_IRREGULAR,
 
     /// <summary>
-    /// <list>
+    /// <list type="bullet">
     ///     <item>Quad</item>
     ///     <item>No side products</item>
     /// </list>
